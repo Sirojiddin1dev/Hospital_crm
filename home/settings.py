33cdf896442d9191b9b721063009f4e6013f670b
a1onedeveloper <141642705+a1onedeveloper@users.noreply.github.com>
2024-02-21 15:54:47 +0500

    sirojiddin

--- home/settings.py
+++ home/settings.py
@@ -105,6 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S",
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
